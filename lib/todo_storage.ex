785f7d532b7b879c9ec5368972c0604e7fd63855
Olga Draber <olgadraber@gmail.com>
2018-07-20 00:13:21 +0200

    add gen server storage for columns and tasks

---
+++ lib/todo_storage.ex
@@ -0,0 +1,54 @@
+defmodule Todo.Storage do
+  use GenServer
+
+  # Client
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def fetch_from_column(server, key) do
+    GenServer.call(server, {:fetch_from_column, key})
+  end
+
+  def fetch(server) do
+    GenServer.call(server, {:fetch})
+  end
+
+  # Server
+
+  def init(:ok) do
+    columns = [
+      %{
+        key: 'to-do',
+        tasks: [
+          %{name: 'some task', key: 1},
+          %{name: 'another task', key: 2}
+        ]
+      },
+      %{
+        key: 'doing',
+        tasks: [
+          %{name: 'working on it now', key: 3}
+        ]
+      },
+      %{
+        key: 'done',
+        tasks: [
+          %{name: 'already done task', key: 4}
+        ]
+      },
+    ]
+    {:ok, columns}
+  end
+
+  def handle_call({:fetch_from_column, key}, _from, columns) do
+    col = Enum.find(columns, fn(col) -> col.key == key end)
+    {:reply, col.tasks, columns}
+  end
+
+  def handle_call({:fetch,}, _from, columns) do
+    tasks = Enum.flat_map(columns, fn(col) -> col.tasks end)
+    {:reply, tasks, columns}
+  end
+end
